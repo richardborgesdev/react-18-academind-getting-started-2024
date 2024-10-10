d3b288e0effb9630c54f99ea4530fac83e9dc470
richardborgesdev <richardborgesdev@gmail.com>
2024-10-10 09:55:39 -0300

    chore: javascript refresh | import and export

---
+++ 03-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,10 @@
+// import {
+//   apiKey,
+//   abc as content, // alias
+// } from './util.js';
+
+// import apiKey from './util.js'; // with default export
+
+import * as util from './util.js'; // with named exports
+
+console.log(util.apiKey, util.abc);
