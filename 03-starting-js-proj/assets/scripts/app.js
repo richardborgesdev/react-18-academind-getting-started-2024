97574e9c8ba87d0c4e499703c3b1d9c21d66582d
richardborgesdev <richardborgesdev@gmail.com>
2024-10-11 12:28:30 -0300

    chore: js refresh | variables, operators

--- 03-starting-js-proj/assets/scripts/app.js
+++ 03-starting-js-proj/assets/scripts/app.js
@@ -1,3 +1,5 @@
+// IMPORT AND EXPORT
+
 // import {
 //   apiKey,
 //   abc as content, // alias
@@ -8,3 +10,12 @@
 import * as util from './util.js'; // with named exports
 
 console.log(util.apiKey, util.abc);
+
+// VARIABLES
+let userMessage = "hello world!!";
+
+console.log(userMessage);
+
+// OPERATORS
+console.log("hello" + "world");
+console.log(10 === 5);
