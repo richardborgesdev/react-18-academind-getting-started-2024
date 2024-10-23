e13f6acce8b3e2fd9b4c632565ff5785551d2465
richardborgesdev <richardborgesdev@gmail.com>
2024-10-23 09:44:44 -0300

    chore: react start: dynamic values, components and props

--- 04-starting-project/src/App.jsx
+++ 04-starting-project/src/App.jsx
@@ -1,14 +1,33 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Cores'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max + 1);
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
+}
+
+function CoreConcepts(props) {
+  return <li>
+    <img src={props.image} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>
 }
 
 function App() {
@@ -16,6 +35,14 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description="The core UI building block." image={componentsImg} />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
