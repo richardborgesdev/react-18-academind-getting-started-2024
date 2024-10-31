a46fb6e5d628518ee29d2dae8993beebd2128215
richardborgesdev <richardborgesdev@gmail.com>
2024-10-31 11:20:53 -0300

    chore: react start | change content based on state

--- 04-starting-project/src/App.jsx
+++ 04-starting-project/src/App.jsx
@@ -1,11 +1,11 @@
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button ');
+  const [selectedTopic, setSelectedTopic] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -37,7 +37,19 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>
+              {EXAMPLES[selectedTopic].title}
+            </h3>
+            <p>
+              {EXAMPLES[selectedTopic].description}
+            </p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
