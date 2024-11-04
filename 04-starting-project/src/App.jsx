28beaf97a48a176b9aaba149adc65ffe977e05c3
richardborgesdev <richardborgesdev@gmail.com>
2024-11-04 11:03:14 -0300

    chore: react start | dynamic content

--- 04-starting-project/src/App.jsx
+++ 04-starting-project/src/App.jsx
@@ -5,11 +5,35 @@ import TabButton from './components/TabButton.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedTopic);
+  }
+
+  let tabContent = (
+    <p>
+      Please select a topic.
+    </p>
+  );
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>
+          {EXAMPLES[selectedTopic].title}
+        </h3>
+        <p>
+          {EXAMPLES[selectedTopic].description}
+        </p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -37,19 +61,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>
-              {EXAMPLES[selectedTopic].title}
-            </h3>
-            <p>
-              {EXAMPLES[selectedTopic].description}
-            </p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
