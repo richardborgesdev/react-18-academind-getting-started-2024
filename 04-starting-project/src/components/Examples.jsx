3f6b6462a68c4dd0b1f746915bf0143d64735ab5
richardborgesdev <richardborgesdev@gmail.com>
2024-11-12 09:16:50 -0300

    chore: react deep dive | props propagation

--- 04-starting-project/src/components/Examples.jsx
+++ 04-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import TabButton from "./TabButton.jsx";
 import { useState } from "react";
 import { EXAMPLES } from "../data";
+import { Section } from "./Section.jsx";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -25,35 +26,34 @@ export default function Examples() {
   }
 
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section id="examples" title="Examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
-          onSelect={() => handleSelect("components")}
+          onClick={() => handleSelect("components")}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "jsx"}
-          onSelect={() => handleSelect("jsx")}
+          onClick={() => handleSelect("jsx")}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "props"}
-          onSelect={() => handleSelect("props")}
+          onClick={() => handleSelect("props")}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "state"}
-          onSelect={() => handleSelect("state")}
+          onClick={() => handleSelect("state")}
         >
           State
         </TabButton>
       </menu>
       {tabContent}
-    </section>
+    </Section>
   );
 }
